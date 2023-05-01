40347250c45e39c3c92dcc069fb6c7f65b379ff7
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-05-01 17:37:53 -0300

    Initial commit, set up spring boot

---
+++ src/main/java/com/chessdbstats/chessdbstats/ChessDbStatsApplication.java
@@ -0,0 +1,13 @@
+package com.chessdbstats.chessdbstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessDbStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessDbStatsApplication.class, args);
+    }
+
+}
