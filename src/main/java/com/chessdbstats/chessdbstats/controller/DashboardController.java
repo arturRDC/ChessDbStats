24ebe93a826381f12c2c333e667adb8c7efdb736
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-05-05 16:53:26 -0300

    Add other pages to the dashboard

--- src/main/java/com/chessdbstats/chessdbstats/controller/DashboardController.java
+++ src/main/java/com/chessdbstats/chessdbstats/controller/DashboardController.java
@@ -9,9 +9,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("dashboard/")
 public class DashboardController {
-    @GetMapping("results/{collectionId}")
+    @GetMapping("{collectionId}")
     String getDashboard(Model model, @PathVariable("collectionId") Integer collectionId) {
         model.addAttribute("collectionId", collectionId);
         return "results";
+    }
+    @GetMapping("results/{collectionId}")
+    String getResults(Model model, @PathVariable("collectionId") Integer collectionId) {
+        model.addAttribute("collectionId", collectionId);
+        return "results";
+    }
+    @GetMapping("logistics/{collectionId}")
+    String getLogistics(Model model, @PathVariable("collectionId") Integer collectionId) {
+        model.addAttribute("collectionId", collectionId);
+        return "logistics";
+    }
+    @GetMapping("players/{collectionId}")
+    String getPlayers(Model model, @PathVariable("collectionId") Integer collectionId) {
+        model.addAttribute("collectionId", collectionId);
+        return "players";
+    }
+    @GetMapping("square-frequency/{collectionId}")
+    String getSquareFrequency(Model model, @PathVariable("collectionId") Integer collectionId) {
+        model.addAttribute("collectionId", collectionId);
+        return "square-frequency";
     }
 }
